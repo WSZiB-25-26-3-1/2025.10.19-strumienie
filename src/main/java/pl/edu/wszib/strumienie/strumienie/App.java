260494c1a333a883c0b9d8d4429d737e94c02fd6
bereda <mat.bereda@gmail.com>
2025-12-21 13:53:48 +0100

    strumienie 2

--- src/main/java/pl/edu/wszib/strumienie/strumienie/App.java
+++ src/main/java/pl/edu/wszib/strumienie/strumienie/App.java
@@ -3,7 +3,10 @@ package pl.edu.wszib.strumienie.strumienie;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class App {
@@ -11,7 +14,27 @@ public class App {
         List<Integer> list = List.of(1,2,3,4,1,5,6,-2,7,4,7,8,9,0,11,12,13);
 
         list.stream()
-                .skip(5)
+                .map(i -> "" + i)
+                .filter(s -> s.startsWith("1"))
+                .peek(System.out::println);
+
+        /*List<Car> cars = List.of(
+                new Car("C1", "C1", new Car.Engine("E1", "X"), List.of("Z", "N", "C")),
+                new Car("C2", "C2", new Car.Engine("E1", "X"), List.of("Z", "N", "C")),
+                new Car("C3", "C3", new Car.Engine("E1", "X"), List.of("Z", "N", "C")),
+                new Car("C4", "C4", new Car.Engine("E1", "X"), List.of("Z", "N", "C")),
+                new Car("C5", "C5", new Car.Engine("E1", "X"), List.of("Z", "N", "C")),
+                new Car("C6", "C6", new Car.Engine("E1", "X"), List.of("Z", "N", "C"))
+        );
+
+        cars.stream()
+                .flatMap(c -> c.getColors().stream())
+                .forEach(System.out::println);*/
+
+        /*cars.stream()
+                .sorted()
                 .forEach(System.out::println);
+
+        System.out.println(cars);*/
     }
 }
